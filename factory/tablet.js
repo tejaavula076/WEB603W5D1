7c5c84e23e1b541905d2f81e30d9e22e92b25c58
= <avula.usedu1@gmail.com>
2026-03-30 18:14:43 -0500

    changes for factory pattern

--- factory/tablet.js
+++ factory/tablet.js
@@ -1,1 +1,7 @@
-
+const Tablet = function ({ ram, ssd, name, network }) {
+  this.ram = ram || 0;
+  this.ssd = ssd || 0;
+  this.name = name || "";
+  this.network = network || 0;
+};
+module.exports = Tablet;
